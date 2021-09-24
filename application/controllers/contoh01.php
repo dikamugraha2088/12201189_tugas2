43719e0cfa8e3a05b7d3f9beed2742a7c8e9870b
WP2-dika20 <dikanugraha2088@gmail.com>
2021-09-24 18:55:22 +0700

    menambahkan framework

---
+++ application/controllers/contoh01.php
@@ -0,0 +1,14 @@
+<?php 
+ class contoh01 extends CI_controller
+ {
+  public function index()
+  {
+   echo"<h1>perkenalan</h1>";
+   echo"Nama saya DIKA NUGRAHA
+        saya tinggal di daerah 
+		kawalu olah raga yang saya 
+		sukai adalah silat 
+		no telp 087700205806":
+		}
+	}
+?>
